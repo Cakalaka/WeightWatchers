17c9e1393ae86b9e04e4ba354524374e5be4d5a4
Christopher Kay <chistopehrkay@gmail.com>
2013-02-28 11:44:30 +0000

    added data sturcture

--- src/WeightWatchers.cpp
+++ src/WeightWatchers.cpp
@@ -14,14 +14,43 @@ int main() {
 	char gender;
 	string description;
 
-	height = 175;
-	weight = 77;
-	gender = 'm';
+	struct personData{
+		double height;
+		double weight;
+		double gender;
+	};
 
-	cout << "Body surface area is " << bodySurfaceArea(height, weight) << endl;
-	cout << "Body mass index is " << bodyMassIndex(height, weight) << endl;
-	cout << "Lean mass index is " << leanMassIndex(height, weight, gender) << endl;
-	cout << "You are currently " << description << endl;
+	personData foo, bar;
+	personData rumple = {175, 77, 'f'};
+
+	foo.height = 175;
+	foo.weight = 77;
+	foo.gender = 'm';
+
+	cout << "Enter a person's height, weight and gender: " ;
+	cin >> bar.height >> bar.weight >> bar.gender;
+	cout << endl;
+
+	cout << "Foo's data is : " << foo.height << " : "
+			<< foo.weight << " : "
+			<< foo.gender << endl;
+
+	cout << "Bar's data is : " << bar.height << " : "
+			<< bar.weight << " : "
+			<< bar.gender << endl;
+
+	cout << "Rumple's data is : " << rumple.height << " : "
+			<< rumple.weight << " : "
+			<< rumple.gender << endl;
+
+	//height = 175;
+	//weight = 77;
+	//gender = 'm';
+
+	//cout << "Body surface area is " << bodySurfaceArea(height, weight) << endl;
+	//cout << "Body mass index is " << bodyMassIndex(height, weight) << endl;
+	//cout << "Lean mass index is " << leanMassIndex(height, weight, gender) << endl;
+	//cout << "You are currently " << description << endl;
 
 	cout << "Hello Christopher" << endl; // prints Hello Christopher
 	return 0;
